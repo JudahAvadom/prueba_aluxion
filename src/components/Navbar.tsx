e59407c1455a5d13ecf33b61f34587af5d40ea47
Alberto Casanova <58746126+JudahAvadom@users.noreply.github.com>
2022-11-17 19:13:40 -0500

    GraphQL Github API implementation

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+
+const Navbar = () => {
+  return (
+    <div className="bg-cyan-600 p-4"> 
+        <nav>
+            <ul>
+                <li className="text-2xl text-white">Buscador de Github</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
